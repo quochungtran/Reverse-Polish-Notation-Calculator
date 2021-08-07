2c084814900a3b9775dc16dd9bd0004657601a5b
quochungtran <quochungtran1999@gmail.com>
2021-08-07 20:05:32 +0200

    test commit

--- build/src/CMakeFiles/calculator_app.dir/calculator_app_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/calculator_app.dir/calculator_app_autogen/mocs_compilation.cpp.o: \
- /home/tranquochung/Desktop/repositories/github/Reverse-Polish-Notation-Calculator/build/src/calculator_app_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/tools/CMakeFiles/testUnit1.dir/testUnit1_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-tools/CMakeFiles/testUnit1.dir/testUnit1_autogen/mocs_compilation.cpp.o: \
- /home/tranquochung/Desktop/repositories/github/Reverse-Polish-Notation-Calculator/build/tools/testUnit1_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
