39cbf3055723b36380a16904730326b3259b4487
TRAN Quoc Hung <quochungtr99@gmail.com>
2021-08-12 23:58:53 +0200

    add LitAtom.cpp

---
+++ src/Litteral/LitAtom.cpp
@@ -0,0 +1,47 @@
+#include "LitAtom.h"
+#include "LitRationelle.h"
+#include "LitReelle.h"
+#include "CalculatorException.h"
+
+bool LitAtom::isNull() const
+{
+    return valeur->isNull();
+}
+
+QString LitAtom::getNom() const
+{
+    return nom;
+}
+
+Litterale *LitAtom::getValeur() const
+{
+    return valeur;
+}
+
+typeLit LitAtom::getType() const
+{
+    return typeLit::Atom;
+}
+
+QString LitAtom::getText()
+{
+    return nom;
+}
+
+LitNumerique *LitAtom::getValeurNumerique()
+{
+    if (valeur->getType() != typeLit::PROG && valeur->getType() != typeLit::EXP)
+        return valeur->getValeurNumerique();
+    else
+        throw CalculatorException("valeur de Litterale atom  invalide");
+}
+
+void LitAtom::setValeur(Litterale *l1)
+{
+    valeur = l1;
+}
+
+void LitAtom::setNom(const QString &c)
+{
+    nom = c;
+}
--- src/Litteral/LitAtom.h
+++ src/Litteral/LitAtom.h
@@ -1,15 +1,16 @@
 #ifndef LitAtom_h
 #define LitAtom_h
 #include <iostream>
-#include "Litterale.h"
 #include <string>
+#include "Litterale.h"
 #include "LitProgramme.h"
+
 class LitAtom : public Litterale
 {
 private:
     QString nom;
     Litterale *valeur;
-    
+
 public:
     LitAtom(const QString &nom1, Litterale *val = nullptr)
         : nom(nom1), valeur(val)
@@ -17,31 +18,20 @@ public:
         if (valeur->getType() == typeLit::Atom || valeur->getType() == typeLit::EXP)
             throw CalculatorException("type Litterale invalide");
     }
-    bool isNull() const { return valeur->isNull(); }
-    //accesseur en lecture
-    QString getNom() const { return nom; }
-    Litterale *getValeur() const { return valeur; }
-    typeLit getType() const override { return typeLit::Atom; }
-    QString getText() { return nom; }
+
+    bool                    isNull()            const;
+    //accesseur en lecture      
+    QString                 getNom()            const;
+    Litterale *             getValeur()         const;
+    typeLit                 getType()           const override;
+    QString                 getText();
     // This function permet de tirer le pointeur Numerique pour des classes contient la valeur Numerique
-    virtual LitNumerique *getValeurNumerique() override
-    {
-        if (valeur->getType() != typeLit::PROG && valeur->getType() != typeLit::EXP)
-            return valeur->getValeurNumerique();
-        else
-            throw CalculatorException("valeur de Litterale atom  invalide");
-    }
+    virtual LitNumerique *getValeurNumerique()  override;
 
-    LitProgramme *getProgramme()
-    {
-        if (valeur->getType() == typeLit::PROG)
-        {
-            return new LitProgramme(valeur->getText());
-        }
-    }
+    LitProgramme *getProgramme();
 
-    void setValeur(Litterale *l1) { valeur = l1; }
-    void setNom(const QString &c) { nom = c; }
+    void setValeur(Litterale *l1);
+    void setNom   (const QString &c); 
 };
 
 #endif
